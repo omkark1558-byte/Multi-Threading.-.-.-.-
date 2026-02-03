5a7b598e7b327a1465e1c387e81f256ad46c2c9b
omkark1558-byte <omkark1558@gmail.com>
2026-02-03 15:37:54 +0530

    Logical

---
+++ Fourth_Sny1.java
@@ -0,0 +1,68 @@
+package multi_threading;
+
+public class Fourth_Sny1 {
+
+	public static void main(String[] args) throws Exception {
+		
+		Count cont = new Count();
+		
+		Number one = new Number();
+		Number_1 two = new Number_1();
+		
+		one.conut=cont;
+		two.conut=cont;
+		 
+		one.start();
+		two.start();
+
+		Thread.sleep(1000);
+		System.out.println(cont.getCount());
+		System.out.println();
+		
+		
+	}
+
+}
+class Count  
+{
+	int count = 0;
+	public void Increase()
+	{
+		this.count++;
+	}
+	
+	public int getCount()
+	{
+		return this.count;
+	}
+}
+
+class Number extends Thread
+{
+	 
+	Count conut;
+//	 Number(Count count){
+//		 this.count = count; 
+				// }
+	 
+	public void run()
+	{
+		for(int i=0; i<10000; i++)
+		{
+			conut.Increase();
+		}
+	}
+	
+}
+
+class Number_1 extends Thread
+{
+	Count conut;
+	public void run()
+	{
+		for(int i=0; i<10000; i++)
+		{
+			conut.Increase();
+		}
+	}
+}
