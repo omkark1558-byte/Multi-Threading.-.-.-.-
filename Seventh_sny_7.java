a945a219f548c6b2c3920210442e74ffe8e622b1
omkark1558-byte <omkark1558@gmail.com>
2026-02-03 15:38:31 +0530

    Logical's

---
+++ Seventh_sny_7.java
@@ -0,0 +1,68 @@
+package multi_threading;
+
+public class Seventh_sny_7 {
+
+	public static void main(String[] args) throws InterruptedException {
+
+		Printer Obj = new Printer();
+		Printer Obj_2 = new Printer();
+		
+		
+		
+		Switch_on ons = new Switch_on(Obj);
+		Switch_of ofs = new Switch_of(Obj_2);
+		
+		ons.start();
+		ofs.start();
+		
+		Thread.sleep(4500);
+		
+		System.out.println(Obj.getPrinter());
+		System.out.println(Obj_2.getPrinter());
+	
+	}
+
+}
+
+class Printer {
+	int counter = 0;
+	int counter_2 = 0;
+	public void Incresed() {
+		synchronized(this) {
+		this.counter++;
+		}
+		this.counter_2 = counter_2;
+	}
+
+	public int getPrinter() {
+		return this.counter;
+	}
+}
+
+class Switch_on extends Thread {
+	Printer counter;
+
+	Switch_on(Printer counter) {
+		this.counter = counter;
+	}
+
+	public void run() {
+		for (int i = 0; i < 1000; i++) {
+			counter.Incresed();
+		}
+	}
+}
+
+class Switch_of extends Thread {
+	Printer counter;
+
+	Switch_of(Printer counter) {
+		this.counter = counter;
+	}
+
+	public void run() {
+		for (int i = 0; i < 1000; i++) {
+			counter.Incresed();
+		}
+	}
+}
